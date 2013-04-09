b3cb32c2dbb0c676db13a205aa9eef91a9673eea
gg <gg@192.168.1.2>
2013-04-09 22:36:29 +0300

    Added splay, add and find methods

--- src/lisouski/dzianis/SplayTree.java
+++ src/lisouski/dzianis/SplayTree.java
@@ -8,16 +8,88 @@ package lisouski.dzianis;
  *
  * @author DenisLesovsky
  */
-public class SplayTree<K, V> {
+public class SplayTree<K extends Comparable<K>, V> {
     
     private Node<K, V> root;
     
-    private static final class Node<K, V> {
+    private static final class Node<K extends Comparable<K>, V> {
         K key;
         V value;
         Node<K, V> left;
         Node<K, V> right;
         Node<K, V> parent;
+        
+        Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+    
+    // Public methods
+    
+    public V insert(K key, V value) {
+        Node newNode = new Node(key, value);
+        
+        if (root == null) {
+            root = newNode;
+            return null;
+        }
+        
+        Node curNode = root;
+        V returnValue = null;
+        
+        for(;;) {
+            int compareResult = curNode.key.compareTo(key);
+            if (compareResult == 0) {
+                if (curNode.parent != null) {
+                    if (curNode.parent.left == curNode) {
+                        curNode.parent.left = newNode;
+                    } else {
+                        curNode.parent.right = newNode;
+                    }
+                }
+                newNode.left = curNode.left;
+                newNode.right = curNode.right;
+                returnValue = (V) curNode.value;
+                break;
+            } else if (compareResult > 0) {
+                if (curNode.left == null) {
+                    curNode.left = newNode;
+                    newNode.parent = curNode;
+                    break;
+                }
+                curNode = curNode.left;
+            } else {
+                if (curNode.right == null) {
+                    curNode.right = newNode;
+                    newNode.parent = curNode;
+                    break;
+                }
+                curNode = curNode.parent;
+            }
+        }
+        
+        splay(newNode);
+        
+        return returnValue;
+    }
+    
+    public V find (K key) {
+        Node curNode = root;
+        V returnValue = null;
+        
+        while (curNode != null) {
+            int compareResult = curNode.key.compareTo(key);
+            if (compareResult == 0) {
+                returnValue = (V) curNode.value;
+            } else if (compareResult > 0) {
+                curNode = curNode.left;
+            } else {
+                curNode = curNode.right;
+            }
+        }
+        
+        return returnValue;
     }
     
     // Rotations
@@ -57,8 +129,29 @@ public class SplayTree<K, V> {
         x.parent = y;
     }
     
-    public void add(K key, V value) {
-        
+    // Splay operation
+    private void splay(Node x) {
+        while (x.parent != null) {
+            if (x.parent.parent == null) { // Zig
+                if (x.parent.left == x) {
+                    rightRotation(x);
+                } else {
+                    leftRotation(x);
+                }
+            } else if (x.parent.left == x && x.parent.parent.left == x.parent) { // ZigZig 1
+                rightRotation(x.parent.parent);
+                rightRotation(x.parent);
+            } else if (x.parent.right == x && x.parent.parent.right == x.parent) { // ZigZig 2
+                leftRotation(x.parent.parent);
+                leftRotation(x.right);
+            } else if (x.parent.left == x) {
+                rightRotation(x.parent);
+                leftRotation(x.parent);
+            } else {
+                leftRotation(x.parent);
+                rightRotation(x.parent);
+            }
+        }
     }
     
 }
